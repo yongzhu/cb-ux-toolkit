1da56d6503836d775b16e30174950e361e81157e
Mason Beard <mason.beard@gmail.com>
2019-03-28 22:05:27 -0400

    fix: builde break 2

--- projects/cut-lib/karma.conf.js
+++ projects/cut-lib/karma.conf.js
@@ -34,8 +34,8 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    logLevel: config.LOG_DEBUG,
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
     singleRun: false
   });
