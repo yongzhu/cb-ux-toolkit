7a4f5f087c282a9db2bb5b0c83396f8be4cbe40e
Mason Beard <mason.beard@gmail.com>
2019-04-30 22:23:59 -0400

    feat: auto color for kpi tile

--- projects/cut-lib/src/lib/kpi-tile/kpi-tile.component.ts
+++ projects/cut-lib/src/lib/kpi-tile/kpi-tile.component.ts
@@ -1,22 +1,60 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
-  selector: 'cut-kpi-tile',
-  templateUrl: './kpi-tile.component.html',
-  styleUrls: ['./kpi-tile.component.scss']
+	selector: 'cut-kpi-tile',
+	templateUrl: './kpi-tile.component.html',
+	styleUrls: ['./kpi-tile.component.scss']
 })
+
 export class CutKpiTileComponent implements OnInit {
+	private _barValue = new BehaviorSubject<number>(null);
+	barStyle: string;
+
 	@Input() title: string;
 	@Input() mainCount: string = '0';
 	@Input() secondaryCount: string;
 	@Input() infoText: string;
 	@Input() showProgressBar: boolean = true;
-	@Input() progressBarValue: number = 0;
+
+	@Input()
+	set progressBarValue(value) {
+		this._barValue.next(value);
+	};
+
+	get progressBarValue() {
+		return this._barValue.getValue();
+	}
+
 	@Input() progressBarMode: string = 'determinate'
+	@Input() progressBarStyle: string = 'primary';
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this._barValue
+			.pipe(
+				takeWhile(() => !this.barStyle)
+			)
+			.subscribe(val => {
+				if (val) {
+					this.setBarColor(val);
+				}
+			});
+	}
 
+	private setBarColor(val: number) {
+		//TODO: Get the rules and colors for the different behaviors
+		if (val < 30) {
+			this.barStyle = 'warn';
+		}
+		else if (val >= 30 && val < 70) {
+			this.barStyle = "accent";
+		}
+		else {
+			this.barStyle = 'primary';
+		}
+	}
 }
